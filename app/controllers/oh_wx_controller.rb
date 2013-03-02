2992f3cf68007e555fb071b13c3bfbc9bfffc03c
S.A. Walters <s.a.walters.jr@gmail.com>
2013-03-01 23:17:26 -0500

    some querying issues with @meso and @outlook, but able to embed ruby successfully; IMAGES WONT DISPLAY need to determine whats wrong

--- app/controllers/oh_wx_controller.rb
+++ app/controllers/oh_wx_controller.rb
@@ -1,6 +1,9 @@
 class OhWxController < ApplicationController
   def Home
 	@event = Event.find(1)
+	@outlook = Outlook.find(1) #where(:event_id => "1")
+	@meso = Meso.find(1) #where(:event_id => "1")
+	
   end
 
   def WxEvents
